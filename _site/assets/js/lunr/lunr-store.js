eb7e433ccee03ab1ffe0a14c5d7fe8f79b48b43f
kyoung <kyoung@kyoungui-MacBookPro.local>
2023-05-03 18:56:08 +0900

    0503 최종3

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "BLOG 카테고리 테스트 ",
+        "excerpt":"자바 카테고리 테스트 입니다. 카테고리는 자바, 테그는 “처음”, “테스트” 입니다.   ","categories": ["BLOG"],
+        "tags": ["처음","테스트"],
+        "url": "/blog/BLOG001/",
+        "teaser": null
+      },{
+        "title": "JAVA 카테고리 테스트",
+        "excerpt":"자바 카테고리 테스트 입니다. 카테고리는 자바, 테그는 “처음”, “테스트” 입니다.   ","categories": ["JAVA"],
+        "tags": ["처음","테스트"],
+        "url": "/java/JAVA001/",
+        "teaser": null
+      }]
